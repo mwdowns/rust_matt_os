8be087538b6e755d725d7f790c3696a14d08456a
mwdowns <mwdowns@gmail.com>
2018-03-12 14:52:52 -0400

    finish first part of tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
+#![feature(lang_items)]
 #![no_std]
+#![no_main]
 
-fn main() {
-    println!("Hello, world!");
+#[lang = "panic_fmt"]
+#[no_mangle]
+pub extern fn rust_begin_panic(_msg: core::fmt::Arguments,
+    _file: &'static str, _line: u32, _column: u32) -> !
+{
+    loop {}
+}
+
+#[no_mangle]
+pub extern fn main() -> ! {
+	loop{}
 }
